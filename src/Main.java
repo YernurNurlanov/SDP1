b52d4eb7a56ea6cf0a5ca997fa4c4439efcdeb76
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-10-02 20:53:28 +0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        FootballLeague league = FootballLeague.getInstance();
+        league.addTeam(new FootballLeague.Team("Barcelona", new OffensiveStrategy().play()));
+        league.addTeam(new FootballLeague.Team("Manchester United", new DefensiveStrategy().play()));
+        for (FootballLeague.Team team : FootballLeague.teams ) {
+            System.out.println(team.getName()+" "+team.getStrategy());
+        }
     }
 }
