ab171858a63fe57f9dd36849a7edaf8cdcb56086
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-10-02 11:32:51 +0600

    Strategy

---
+++ src/DefensiveStrategy.java
@@ -0,0 +1,6 @@
+public class DefensiveStrategy implements FootballStrategy{
+    @Override
+    public void play() {
+        System.out.println("the team plays defensive football");
+    }
+}
---
+++ src/OffensiveStrategy.java
@@ -0,0 +1,6 @@
+public class OffensiveStrategy implements FootballStrategy{
+    @Override
+    public void play() {
+        System.out.println("the team plays offensive football");
+    }
+}
